359248e13b9801115fef3f86f6fb73fec7221603
PrathamRa0 <pr14rao@gmail.com>
2022-07-06 13:57:05 +0530

    Updated faces-train.py

--- src/faces-train.py
+++ src/faces-train.py
@@ -2,12 +2,14 @@ import os
 import cv2
 import numpy as np
 from PIL import Image
+import pickle
 
 face_cascade = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_alt2.xml')
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) #Points to the base directory
 image_dir = os.path.join(BASE_DIR, "images")  #Accesses the images folder
 
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 curr_id = 0  #To get to know the id of each label
 label_id = {} #To get all the labels and store it.
@@ -27,13 +29,17 @@ for root, dirs, files in os.walk(image_dir):
                 curr_id += 1
             
             id_ = label_id[label]
-            print(label_id)
+            #print(label_id)
             #print(label, path)
             #Appending into arrays
 
             pil_image = Image.open(path).convert("L") #This will convert images into grayscale
-            image_array = np.array(pil_image, "uint8")
-            print(image_array)
+            #Resizing images for training
+            size = (550, 550)
+            final_image = pil_image.resize(size, Image.ANTIALIAS)
+
+            image_array = np.array(final_image, "uint8")
+            #print(image_array)
 
             faces = face_cascade.detectMultiScale(image_array, scaleFactor = 1.5, minNeighbors = 5)
 
@@ -42,5 +48,15 @@ for root, dirs, files in os.walk(image_dir):
                 x_train.append(roi) #Appends the region which is to be trained in numpy array format.
                 y_labels.append(id_) #Gets the ids of all labels
 
-print(x_train)
-print(y_labels)
+#print(x_train)
+#print(y_labels)
+
+#Pickle module used for serialization. It saves the code in computer readable format.
+#Here we will use pickle to save label IDs.
+#Dump and load are the 2 main functions provided by this module
+
+with open("labels.pickle", "wb") as f:   #wb is used as 'write byte'
+    pickle.dump(label_id, f)
+
+recognizer.train(x_train, np.array(y_labels)) #Training x_train with y_labels. We convert y_label to numpy array before that
+recognizer.save("trainner.yml")
--- src/faces.py
+++ src/faces.py
@@ -1,7 +1,17 @@
 import cv2 
 import numpy as np
+import pickle
 
 face_cascade = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+labels={"person_name": 1}
+with open("labels.pickle", "rb") as f:   #wb is used as 'write byte'
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
+
+
 cap = cv2.VideoCapture(0) #Accesses the main camera of the system
 
 while True:
@@ -9,14 +19,24 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
     for (x,y,w,h) in faces:
-        print(x,y,w,h)
+        #print(x,y,w,h)
         roi_gray = gray[y:y+h, x:x+w] #Roi-> Region of Interest, only the region we need in whole image
         roi_color = frame[y:y+h, x:x+w]
 
+        #Recognizer, Can also be done by DL using tensorflow or keras but here we will use only opencv
+        id_, conf = recognizer.predict(roi_gray) #Conf is confidence of how true it is.
+        if conf>=45:  #Its usually from 0-100 but sometimes it crosses 100 and to prevent that we set range to be 45-85
+            #print(id_)
+            #print(labels[id_])
+            #Putting font on the around the roi area
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
 
-         
-        img_item = "image_try.png"
-        cv2.imwrite(img_item, roi_gray)
+        img_item = "1.png"
+        cv2.imwrite(img_item, roi_color)
 
         color = (255, 0, 0) #OpenCV is BGR and not RGB
         stroke = 2
